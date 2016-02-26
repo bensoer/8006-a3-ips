a2f96e6042623a5d449c6dca832329ed15a66f18
bensoer <ben@soernet.ca>
2016-02-26 15:20:33 -0800

    added framework files for remaining unit tests

---
+++ tests/RecordTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bensoer
+ * Date: 26/02/16
+ * Time: 3:20 PM
+ */
+class RecordTest extends PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/SettingsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bensoer
+ * Date: 26/02/16
+ * Time: 3:19 PM
+ */
+class SettingsTest extends PHPUnit_Framework_TestCase
+{
+
+}
